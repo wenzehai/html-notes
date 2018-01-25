bbc58808738104dcb7030343bc52204d46fcc923
zehai <373952776@qq.com>
2018-01-25 16:12:42 +0800

    海底捞样式修改

--- projects/haidilao/js/screenOrientationListen.js
+++ projects/haidilao/js/screenOrientationListen.js
@@ -1,3 +1,29 @@
+window.onload = function(){
+	var conBd = document.getElementsByClassName('con_bd')[0];
+    // 如果有conBd元素时才执行屏幕检测
+    if(conBd){
+    	var bH = document.body.clientHeight;
+    	var tableBox = document.getElementsByClassName('table_box')[0];
+    	if(tableBox){
+            tableBox.style.height = bH - 267 + 'px';					
+        }else {
+        	conBd.style.height = bH - 267 + 'px';	
+        }
+    }
+    
+    
+    //判断浏览器版本
+    
+	    
+    
+}
+
+//禁止长按弹出右键菜单，貌似没有起作用,有可能是浏览器兼容问题
+window.addEventListener('contextmenu', function(e){  
+    e.preventDefault();  
+});
+
+
 //通过监测横竖屏来动态改变con_bd的高度
 //屏幕方向标识，0横屏，其他值竖屏
 var orientation = 0;
@@ -10,22 +36,27 @@ function screenOrientationEvent() {
     // 如果有conBd元素时才执行屏幕检测
     if(conBd){
         if (orientation == 0) {
-
-            var cH = document.body.clientHeight;
+            var bH = document.body.clientHeight;
+            
             var tableBox = document.getElementsByClassName('table_box')[0];
             //为了解决表格列表过长不能滚动，进行判断，并将高度进行屏幕匹配。
             if(tableBox){
-                tableBox.style.height = cH - 295 + 'px';
+                tableBox.style.height = bH - 267 + 'px';					//295
             }
-            conBd.style.height = cH - 295 + 'px';
+            conBd.style.height = bH - 267 + 'px';
         }
         else {
-            var cH = document.body.clientHeight;
+            var bH = document.body.clientHeight;
+            
+//          alert('浏览器的屏幕宽度是'+cW + '浏览器的屏幕高度是'+cH + '浏览器的body宽度是'+bW + '浏览器的body高度是'+bH);
+//          var cH = document.body.offsetHeight;
+//          var cH = document.documentElement.clientHeight;
+//          alert(cH);
             var tableBox = document.getElementsByClassName('table_box')[0];
             if(tableBox){
-                tableBox.style.height = cH - 295 + 'px';
+                tableBox.style.height = bH - 267 + 'px';
             }
-            conBd.style.height = cH - 295 + 'px';
+            conBd.style.height = bH - 267 + 'px';
         }
     }
 }
@@ -54,3 +85,4 @@ function screenOrientationListener() {
 
 //启动横竖屏事件监听
 screenOrientationListener();
+
